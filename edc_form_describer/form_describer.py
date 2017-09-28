1de7c760a83d0fac05ca491ab317119518f93eb9
erikvw <ew2789@gmail.com>
2017-09-29 00:18:32 +0200

    add handling for custom modeladmin features

--- edc_form_describer/form_describer.py
+++ edc_form_describer/form_describer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from edc_base.fieldsets.fieldsets import Fieldsets
 from edc_base.model_mixins.constants import DEFAULT_BASE_FIELDS
 from math import floor
 
@@ -12,15 +13,40 @@ class FormDescriber:
         describer = FormDescriber(
                 admin_cls=MyModelAdmin,
                 include_hidden_fields=True)
+
+        # get the markdown text as a list of lines
+        markdown_lines = describer.markdown
+
+        # get the markdown as text
         markdown = describer.to_markdown(title='Forms', add_timestamp=True)
+
+        # or write markdown text directly to file
+        describer.to_file(path=path, title='Forms', add_timestamp=True)
+
     """
 
-    def __init__(self, admin_cls=None, include_hidden_fields=None):
+    def __init__(self, admin_cls=None, include_hidden_fields=None, visit_code=None):
         self.markdown = []
         self.admin_cls = admin_cls
         self.model_cls = admin_cls.form._meta.model
+        self.visit_code = visit_code
         self.models_fields = {
             fld.name: fld for fld in self.model_cls._meta.get_fields()}
+
+        # include custom labels from admin
+        self.custom_form_labels = {k: v for k, v in [
+            (form_label.field, form_label.label)
+            for form_label in self.admin_cls.custom_form_labels]}
+
+        # include custom fieldsets from admin if visit_code
+        self.fieldsets = self.admin_cls.fieldsets
+        if self.admin_cls.conditional_fieldsets.get(self.visit_code):
+            self.conditional_fieldset = self.admin_cls.conditional_fieldsets.get(
+                self.visit_code)
+            fieldsets = Fieldsets(self.admin_cls.fieldsets)
+            fieldsets.add_fieldset(fieldset=self.conditional_fieldset)
+            self.fieldsets = fieldsets.fieldsets
+
         self.describe()
         if include_hidden_fields:
             self.add_hidden_fields()
@@ -29,14 +55,22 @@ class FormDescriber:
         """Appends all form features to a list `lines`.
         """
         number = 0.0
-        self.markdown.append(f'## {self.model_cls._meta.verbose_name}')
-        self.markdown.append(self.model_cls.__doc__)
+        verbose_name = self.model_cls._meta.verbose_name
+        if self.visit_code and self.conditional_fieldset:
+            verbose_name = f'{verbose_name} ({self.visit_code})'
+        self.markdown.append(f'## {verbose_name}')
+        docstring = self.model_cls.__doc__
+        if docstring.lower().startswith(self.model_cls._meta.label_lower.split('.')[1]):
+            self.markdown.append('*[missing model class docstring]*\n\n')
+        else:
+            self.markdown.append(self.model_cls.__doc__)
         self.markdown.append(
             f'*Instructions*: {self.admin_cls.instructions}\n')
-        self.markdown.append(
-            f'*Additional instructions*: {self.admin_cls.additional_instructions}\n')
+        if self.admin_cls.additional_instructions:
+            self.markdown.append(
+                f'*Additional instructions*: {self.admin_cls.additional_instructions}\n')
 
-        for fieldset_name, fields in self.admin_cls.fieldsets:
+        for fieldset_name, fields in self.fieldsets:
             if fieldset_name not in ['Audit']:
                 fieldset_name = fieldset_name or 'Main'
                 self.markdown.append(f'\n**Section: {fieldset_name}**')
@@ -82,6 +116,9 @@ class FormDescriber:
         if field_cls.help_text:
             self.markdown.append(
                 f'\n&nbsp;&nbsp;&nbsp;&nbsp; *{field_cls.help_text}*')
+        if self.custom_form_labels.get(fname):
+            self.markdown.append(
+                f'* custom_prompt: *{self.custom_form_labels.get(fname)}*')
         self.markdown.append(f'* db_table: {self.model_cls._meta.db_table}')
         self.markdown.append(f'* column: {field_cls.name}')
         self.markdown.append(f'* type: {field_cls.get_internal_type()}')
