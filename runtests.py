757eccccf7a9ae788ee4dfa002549371efa9a7f8
erikvw <ew2789@gmail.com>
2021-09-11 13:23:14 -0500

    test setup

--- runtests.py
+++ runtests.py
@@ -6,6 +6,7 @@ from os.path import abspath, dirname, join
 import django
 from django.conf import settings
 from django.test.runner import DiscoverRunner
+from edc_constants.constants import IGNORE
 from edc_test_utils import DefaultTestSettings
 
 app_name = "edc_form_describer"
@@ -16,6 +17,9 @@ DEFAULT_SETTINGS = DefaultTestSettings(
     APP_NAME=app_name,
     BASE_DIR=base_dir,
     ETC_DIR=join(base_dir, app_name, "tests", "etc"),
+    EDC_NAVBAR_VERIFY_ON_LOAD=IGNORE,
+    EDC_AUTH_SKIP_SITE_AUTHS=True,
+    EDC_AUTH_SKIP_AUTH_UPDATER=True,
     INSTALLED_APPS=[
         "django.contrib.admin",
         "django.contrib.auth",
